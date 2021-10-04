d9a8475c027c7e9d055a6b5972cac0bced3534f5
Duong Hoang Thach <boduong95@gmail.com>
2021-10-04 17:30:15 +0700

    Install react-router, add routes

---
+++ src/screens/ListScreen.tsx
@@ -0,0 +1,10 @@
+import { type } from 'os';
+import React from 'react';
+
+type Props = {};
+
+const ListScreen: React.FC<Props> = () => {
+  return <div>List view 2</div>;
+};
+
+export default ListScreen;
