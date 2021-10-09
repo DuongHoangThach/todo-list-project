2dd539e39c6d23fa923c921eebb049e016feb450
Duong Hoang Thach <boduong95@gmail.com>
2021-10-10 00:00:19 +0700

    Add useStore hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,39 @@
-import { shuffle } from 'lodash';
-import { nanoid } from 'nanoid';
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
-import useLocalStorage from './hook/use-local-storage';
+import TaskContext from './contexts/task-store';
+import useLocalStorage from './hooks/use-local-storage';
 import FocusScreen from './screens/FocusScreen';
 import ListScreen from './screens/ListScreen';
 import { Task } from './types';
 
 function App() {
   const [tasks, setTasks] = useLocalStorage<Task[]>('tasks', []);
-  const [focusedTaskId, setFocusedTaskId] = useState<string | undefined>(
-    undefined
-  );
-
-  const addTask = (task: Pick<Task, 'label'>) => {
-    const id = nanoid();
-    setTasks((tasks) => [
-      ...tasks,
-      { id, label: task.label, isComplete: false },
-    ]);
-    if (!focusedTaskId) setFocusedTaskId(id);
-  };
-
-  const updateTaskCompletion = (taskId: string, isComplete: boolean) => {
-    setTasks((tasks) =>
-      tasks.map((task) => {
-        if (task.id === taskId) return { ...task, isComplete };
-        return task;
-      })
-    );
-  };
-
-  const focusedTask = tasks.find((task) => task.id === focusedTaskId);
-
-  const shuffleFocusedTask = () => {
-    setFocusedTaskId(shuffle(tasks.filter((task) => !task.isComplete))[0]?.id);
-  };
-
-  const tasksApi = {
-    addTask,
-    focusedTask,
-    tasks,
-    setTasks,
-    shuffleFocusedTask,
-    updateTaskCompletion,
-  };
 
   return (
     <BrowserRouter>
-      <nav>
-        <NavLink exact to="/" activeStyle={{ fontWeight: 'bold' }}>
-          List
-        </NavLink>{' '}
-        - {''}
-        <NavLink to="/focus" activeStyle={{ fontWeight: 'bold' }}>
-          Focus
-        </NavLink>
-      </nav>
-      <br />
+      <TaskContext.Provider value={[tasks, setTasks]}>
+        <nav>
+          <NavLink exact to="/" activeStyle={{ fontWeight: 'bold' }}>
+            List
+          </NavLink>{' '}
+          - {''}
+          <NavLink to="/focus" activeStyle={{ fontWeight: 'bold' }}>
+            Focus
+          </NavLink>
+        </nav>
+        <br />
 
-      <Switch>
-        <Route exact path="/">
-          <div>
-            <ListScreen {...tasksApi} />
-          </div>
-        </Route>
-        <Route path="/focus">
-          <FocusScreen {...tasksApi} />
-        </Route>
-      </Switch>
+        <Switch>
+          <Route exact path="/">
+            <div>
+              <ListScreen />
+            </div>
+          </Route>
+          <Route path="/focus">
+            <FocusScreen />
+          </Route>
+        </Switch>
+      </TaskContext.Provider>
     </BrowserRouter>
   );
 }
